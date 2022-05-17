9add8e32a83ef478bd21dd82754c23f093152128
Teddy Milford <teddy.milford@gmail.com>
2022-05-17 16:27:08 -0600

    commit tree cee1e8dadb4d8014b8065af7c57637a686787ab7

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution here
+
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+const gosling = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+
+const findMatching = function (arr, string) {
+  const response = arr.filter(
+    (word) => word == string || word == string.toLowerCase()
+  );
+  return response;
+};
+
+const fuzzyMatch = function (arr, string) {
+  const response = arr.filter((word) => word[0] == string[0]);
+  return response;
+};
+
+const matchName = function (obj, string) {
+  const response = obj.filter((word) => word.name == string);
+  return response;
+};
